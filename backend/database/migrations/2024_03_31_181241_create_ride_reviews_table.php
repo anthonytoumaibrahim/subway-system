a8b1e9fcaac4ca3a076f09482ede8e21c47f11d4
AliAbdelAall <3laych@gmail.com>
2024-03-31 22:52:07 +0300

    created migration tables

---
+++ backend/database/migrations/2024_03_31_181241_create_ride_reviews_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ride_reviews', function (Blueprint $table) {
+            $table->id();
+            $table->integer("rating");
+            $table->text("text");
+            $table->unsignedBigInteger("user_id");
+            $table->foreign("user_id")->references("id")->on("users");
+            $table->unsignedBigInteger("ride_id");
+            $table->foreign("ride_id")->references("id")->on("rides");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ride_reviews');
+    }
+};
