09c19d13f89edfdb34f4e755fcbff8f0c7ec6443
AliAbdelAall <3laych@gmail.com>
2024-03-31 23:37:53 +0300

    added Ride model

--- backend/app/Models/Ride.php
+++ backend/app/Models/Ride.php
@@ -9,5 +9,21 @@ class Ride extends Model
 {
     use HasFactory;
 
-   
+    protected $fillable = [
+        "departure_date",
+        "arrival_date",
+        "price",
+        "departure_station_id",
+        "arrival_station_id"
+    ];
+
+    public function reviews()
+    {
+        return $this->hasMany(Reviews::class);
+    }
+
+    public function user()
+    {
+        return $this->hasMany(user::class);
+    }
 }
