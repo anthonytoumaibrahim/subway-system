c1c8496629437e2a81c9f610d00cc74248db15c4
atian155 <atianthony@outlook.com>
2024-04-01 20:52:04 +0300

    Modified Login/Signup code

--- frontend/src/pages/users/components/Header/index.jsx
+++ frontend/src/pages/users/components/Header/index.jsx
@@ -1,164 +1,185 @@
-import React, { useState, useEffect } from 'react'
-import { useLocation, Link } from 'react-router-dom'
-import Login from '../Login'
-import Signup from '../Signup'
-import "./style.css"
+import React, { useState, useEffect } from "react";
+import { useLocation, Link } from "react-router-dom";
+import Login from "../Login";
+import Signup from "../Signup";
+import "./style.css";
 
 const Header = () => {
   const initialCredentials = {
     username: "",
     email: "",
     password: "",
-    confirmPassword: ""
-  }
+    confirmPassword: "",
+  };
 
   const initailtErrors = {
     all: false,
     email: false,
-    password: false
-  }
+    password: false,
+    username: false,
+  };
 
-  const location = useLocation()
-  const [error, setError] = useState({status:false, fields: []})
-  const [errorMessage, setErrorMessage] = useState('incorrect')
-  const [isLogin, setIsLogin] = useState(false)
-  const [credentials, setcredentials] = useState(initialCredentials)
-  const [isSignup, setIsSignup] = useState(false)
+  const location = useLocation();
+  const [error, setError] = useState(initailtErrors);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [credentials, setcredentials] = useState(initialCredentials);
+  const [isLogin, setIsLogin] = useState(false);
+  const [isSignup, setIsSignup] = useState(false);
 
-
-  useEffect(() => {resetErrors()}, [credentials])
-  
+  useEffect(() => {
+    resetErrors();
+  }, [credentials]);
 
   const handleInputChange = (value, field) => {
-    console.log (field , value)
-    setcredentials({...credentials, [field]: value })
-    
-  }
+    setcredentials({ ...credentials, [field]: value });
+  };
 
   const resetCredentials = () => {
-    setcredentials({...initialCredentials})
-  }
+    setcredentials({ ...initialCredentials });
+  };
 
   const resetErrors = () => {
-    setError({...initailtErrors})
-  }
+    setError({ ...initailtErrors });
+  };
 
   const handleSwitch = () => {
-    if(isLogin){
-      setIsLogin(false)
-      setIsSignup(true)
-    }else if(isSignup){
-      setIsLogin(true)
-      setIsSignup(false)
+    if (isLogin) {
+      setIsLogin(false);
+      setIsSignup(true);
+    } else if (isSignup) {
+      setIsLogin(true);
+      setIsSignup(false);
     }
-    resetErrors()
-    resetCredentials()
-  }
+    setErrorMessage(null);
+    resetErrors();
+    resetCredentials();
+  };
 
   const handleLoginClick = () => {
-    setIsLogin(true)
-  }
+    setIsLogin(true);
+  };
 
   const handleSignupClick = () => {
-    setIsSignup(true)
-  }
+    setIsSignup(true);
+  };
 
-  const checkEmptyFields = () => {
-    const {username, email, password, confirmPassword} = credentials
-    const regex = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/
-    if(isLogin){
-      if(!email || !password){
-          setError({...error, all: true})
-          setErrorMessage("Fill required fields.")
-          return true
-        }
-    }else if(isSignup){
-      if(!email || !password || !username || !confirmPassword){
-          setError({...error, all: true})
-          setErrorMessage("Fill required fields.")
-          return true
-        }
-    }
-
-    if(!regex.test(email)){
-      setError({...error, email: true})
-      setErrorMessage("Invalid Email.")
-      return true
-    }
-    if (isSignup){
-      if(password !== confirmPassword){
-        setError({...error, password: true})
-        setErrorMessage("passwords does not match.")
-        return true
+  const validateRegistration = () => {
+    setErrorMessage(null);
+    setError(initailtErrors);
+    const { username, email, password, confirmPassword } = credentials;
+    // Sign up Validation
+    if (isSignup) {
+      if (username.trim().length < 6) {
+        setError({ ...error, username: true });
+        setErrorMessage(
+          "Please make sure your username is at least 6 characters long."
+        );
+        return;
+      }
+      if (password !== confirmPassword) {
+        setError({ ...error, password: true });
+        setErrorMessage("Passwords does not match.");
+        return;
       }
     }
-    return false
-  } 
-  
-  const validateRegistration = () => {
-    const empty  = checkEmptyFields()
-    if (empty){
-      console.log("i am empty")
-    }else{
-      console.log("i am not emty")
-      
+    // Validate email
+    if (
+      !/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(email)
+    ) {
+      setError({
+        ...error,
+        email: true,
+      });
+      setErrorMessage("Invalid Email.");
+      return;
     }
-  }
+    // Validate password
+    if (password.length < 8) {
+      setError({
+        ...error,
+        password: true,
+      });
+      setErrorMessage("Password must be at least 8 characters long.");
+      return;
+    }
 
+    console.log("Everything works");
+  };
 
   return (
-    <div className='flex align-center space-between header bg-dark-gray-col'>
-      {isSignup && (<Signup
-      isSignup={isSignup}
-      setIsSignup={setIsSignup}
-      error={error}
-      errorMessage={errorMessage}
-      handleInputChange={handleInputChange}
-      handleSwitch={handleSwitch}
-      validateRegistration={validateRegistration}
-      />)}
-      {isLogin && (<Login
-      isLogin={isLogin}
-      setIsLogin={setIsLogin}
-      error={error}
-      errorMessage={errorMessage}
-      handleInputChange={handleInputChange}
-      handleSwitch={handleSwitch}
-      validateRegistration={validateRegistration}
-      />)}
-      
+    <div className="flex align-center space-between header bg-dark-gray-col">
+      {isSignup && (
+        <Signup
+          credentials={credentials}
+          isSignup={isSignup}
+          setIsSignup={setIsSignup}
+          error={error}
+          errorMessage={errorMessage}
+          handleInputChange={handleInputChange}
+          handleSwitch={handleSwitch}
+          validateRegistration={validateRegistration}
+        />
+      )}
+      {isLogin && (
+        <Login
+          credentials={credentials}
+          isLogin={isLogin}
+          setIsLogin={setIsLogin}
+          error={error}
+          errorMessage={errorMessage}
+          handleInputChange={handleInputChange}
+          handleSwitch={handleSwitch}
+          validateRegistration={validateRegistration}
+        />
+      )}
 
-      <h1 className='logo font-bold white'>Metro<span className='logo text-primary'>Hub</span></h1>
+      <h1 className="logo font-bold white">
+        Metro<span className="logo text-primary">Hub</span>
+      </h1>
 
-      <nav className='flex header-nav'>
-        <Link 
-        to="/" 
-        className={`${location.pathname === "/" ? "active" : ""}`}
-        >Home</Link>
+      <nav className="flex header-nav">
+        <Link to="/" className={`${location.pathname === "/" ? "active" : ""}`}>
+          Home
+        </Link>
 
-        <Link 
-        to="/my-rides"
-        className={`${location.pathname === "/my-rides" ? "active" : ""}`}
-        >My Rides</Link>
+        <Link
+          to="/my-rides"
+          className={`${location.pathname === "/my-rides" ? "active" : ""}`}
+        >
+          My Rides
+        </Link>
 
-        <Link 
-        to="/coins"
-        className={`${location.pathname === "/coins" ? "active" : ""}`}
-        >Coins</Link>
+        <Link
+          to="/coins"
+          className={`${location.pathname === "/coins" ? "active" : ""}`}
+        >
+          Coins
+        </Link>
 
-        <Link 
-        to="/chat"
-        className={`${location.pathname === "/chat" ? "active" : ""}`}
-        >Chat</Link>
+        <Link
+          to="/chat"
+          className={`${location.pathname === "/chat" ? "active" : ""}`}
+        >
+          Chat
+        </Link>
       </nav>
 
-      <div className='flex bold register-btns'>
-        <button className='reg-btn bg-primary font-bold white' onClick={handleLoginClick}>Login</button>
-        <button className='reg-btn signup-btn text-primary font-bold' onClick={handleSignupClick}>Signup</button>
+      <div className="flex bold register-btns">
+        <button
+          className="reg-btn bg-primary font-bold white"
+          onClick={handleLoginClick}
+        >
+          Login
+        </button>
+        <button
+          className="reg-btn signup-btn text-primary font-bold"
+          onClick={handleSignupClick}
+        >
+          Signup
+        </button>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- frontend/src/pages/users/components/RegisterInput/index.jsx
+++ frontend/src/pages/users/components/RegisterInput/index.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
-import "./style.css"
+import React from "react";
+import "./style.css";
 
-const Input = ({id, field, placeholder, label, error, handleInputChange}) => {
+const Input = ({
+  id,
+  value,
+  field,
+  placeholder,
+  label,
+  error,
+  handleInputChange,
+}) => {
   return (
-    <div className='flex column reg-input-wrapper'>
-      <label className='white ' htmlFor={`${id}`}>{`${label}`}</label>
-      <input 
-      className={`bg-dark-gray-col ${error ? "red-border":"black-border"} white`}
-      type="text" placeholder={`${placeholder}`} id={`${id}`}
-      onChange={(e) => 
-        handleInputChange(e.target.value, field)
-        }
-        />
+    <div className="flex column reg-input-wrapper">
+      <label className="white " htmlFor={`${id}`}>{`${label}`}</label>
+      <input
+        value={value}
+        className={`bg-dark-gray-col ${
+          error ? "red-border" : "black-border"
+        } white`}
+        type="text"
+        placeholder={`${placeholder}`}
+        id={`${id}`}
+        onChange={(e) => handleInputChange(e.target.value, field)}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
