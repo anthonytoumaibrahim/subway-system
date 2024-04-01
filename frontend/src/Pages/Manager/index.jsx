163b65ee3293ba619f908024a985ad65e00627dd
MaryamHmayed <mariam.hmayed.88@gmail.com>
2024-04-02 01:25:39 +0300

    station-info

--- frontend/src/Pages/Manager/index.jsx
+++ frontend/src/Pages/Manager/index.jsx
@@ -1,18 +1,44 @@
 import './manager.css';
 import React from "react";
-import { useNavigate } from 'react-router-dom';
 import "./components/Rides";
 import Sidebar from './components/Sidebar';
-
+import './station.css';
 
 
 
 const Manager = () =>{
    return(
-    <>
+    <><div className='Station-container flex '>
+        
         <Sidebar/>
-        <h1>Station info</h1>
+        <div className='station-info-section flex column gap-20'>
+            <h4 className='station-name text-center text-primary'>Station name</h4>
+
+            <table class="station-table white">
+          <thead>
+            <tr>
+              <th>Opening time</th>
+              <th>Closing time</th>
+              <th>Facilities</th>
+              <th>Service status</th>
+              <th>Edit</th>
+              
+            </tr>
+          </thead>
+          <tbody className='station-data'>
+            <td>11:00 AM</td>
+            <td>20:00 PM</td>
+            <td>Wifi access</td>
+            <td>Active</td>
+            <td>edit</td>
+          </tbody>
+        </table>
+
+        </div>
+       
     
+        
+        </div>
     </>
    )
 }
