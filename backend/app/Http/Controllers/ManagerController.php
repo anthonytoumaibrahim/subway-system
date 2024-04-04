b2d8035e9da3f5bd7c1d52426d47c138ad151795
atian155 <atianthony@outlook.com>
2024-04-04 22:33:15 +0300

    Fixed manager station update API

--- backend/app/Http/Controllers/ManagerController.php
+++ backend/app/Http/Controllers/ManagerController.php
@@ -12,9 +12,10 @@ use App\Models\Station;
 
 class ManagerController extends Controller
 {
-    public function getStationInfo(Request $req){
-    
-        $id=$req->id;
+    public function getStationInfo(Request $req)
+    {
+
+        $id = $req->id;
         $station = Station::with(['facilities'])->find($id);
         if (!$station) {
             return response()->json([
@@ -29,82 +30,87 @@ class ManagerController extends Controller
         // $status = $station->status;
         // $facilities= $station->facility->where;
 
-        
-        
-        return response()->json([
-            'success'=> true,
-            'station'=> $station,
-            
 
-          
+
+        return response()->json([
+            'success' => true,
+            'station' => $station,
+
+
+
         ]);
     }
-    public function updateStationInfo(Request $req){
-        $id=$req->id;
-        $station = Station::with('facilities:id,name')->find($id);
+    public function updateStationInfo(Request $req)
+    {
+        $req->validate([
+            "id" => "required|exists:stations,id"
+        ]);
+        $id = $req->id;
+        $station = Station::find($id);
 
         $station->update([
-            'opening' =>$req ->opening,
-            'closing' =>$req->closing,
-            'status'=>$req -> status
-    
+            'opening' => $req->opening,
+            'closing' => $req->closing,
+            'status' => $req->status
         ]);
+
+        if ($req->facilities) {
+            foreach ($req->facilities as $facility) {
+                $station->facilities()->where('id', $facility['id'])->update([
+                    'name' => $facility['name']
+                ]);
+            }
+        }
+
+
 
         return response()->json([
             "message" => "updated successfully"
         ], 201);
-
     }
-    
 
-    public function createRide(Request $req){
+
+    public function createRide(Request $req)
+    {
 
         $station = Station::find($req->arrival_station_id);
 
         if (!$station) {
-        return response()->json([
-            'error' => true,
-            'message' => 'Destination station not found.'
-        ], 404);
+            return response()->json([
+                'error' => true,
+                'message' => 'Destination station not found.'
+            ], 404);
         };
         $destination = $station->name;
 
         $ride = Ride::create([
-            "departure_date" => $req -> departure_date,
-            "arrival_date" => $req-> arrival_date,
-            "price" => $req -> price,
-            "destination"=> $req -> $destination
+            "departure_date" => $req->departure_date,
+            "arrival_date" => $req->arrival_date,
+            "price" => $req->price,
+            "destination" => $req->$destination
 
         ]);
         return response()->json([
             "message" => "created successfully"
         ], 201);
-
     }
 
 
 
 
 
-    public function getRideInfo(Request $req){
+    public function getRideInfo(Request $req)
+    {
         $ride = Ride::find($req->id);
 
         return response()->json([
             "message" => "found successfully",
             "ride" => $ride
         ], 200);
-
-
-
     }
 
 
-    public function updateRideInfo(Request $req){
-
+    public function updateRideInfo(Request $req)
+    {
     }
-
-
-
-
-
 }
